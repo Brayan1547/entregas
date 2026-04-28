ca2148cb6c6014a484edbf63fbed24466d20a955
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-04-27 21:23:53 -0500

    error al conectarse al fakeAPI

--- enClases/lunes27abril/01-fake-api.js
+++ enClases/lunes27abril/01-fake-api.js
@@ -1,9 +1,16 @@
 const API = "https://fakeapi.net/products"
 
-async function getProducts(){
-    const response = await fetch (API)
-    const data = await response.json ()
-    console.log(data)
+async function getProducts() {
+    try {
+        const response = await fetch(API);
+        const data = await response.json();
+
+        console.log(data);
+
+    } catch (error) {
+        console.error("error al conectarse al FakeAPI");
+    }
+
 }
-getProducts()
+getProducts();
 
