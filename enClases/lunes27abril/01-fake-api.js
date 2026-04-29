971fcd6351ad68ca7b6899821bb34c69ef25b36b
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-04-28 21:58:12 -0500

    ejercicio en clase martes 28abril

--- enClases/lunes27abril/01-fake-api.js
+++ enClases/lunes27abril/01-fake-api.js
@@ -1,3 +1,26 @@
+// const API = "https://fakeapi.net/products"
+
+// async function getProducts() {
+//     try {
+//         const response = await fetch(API);
+//         const data = await response.json();
+
+//         let total = 0;
+//         for  ( const producto of data.data){
+//             total = total + producto.price;
+//         }
+
+
+//        console.log(total);
+
+//     } catch (error) {
+//         console.error("error al conectarse al FakeAPI");
+//     }          
+// }
+// getProducts();
+
+
+
 const API = "https://fakeapi.net/products"
 
 async function getProducts() {
@@ -5,16 +28,20 @@ async function getProducts() {
         const response = await fetch(API);
         const data = await response.json();
 
-        let total = 0
+        let total = 0;
         for  ( const producto of data.data){
-            total = total + producto.price;
+            console.log(producto.title, producto.price * producto.stock);
+            total = total + (producto.price * producto.stock);
         }
+
 
        console.log(total);
 
     } catch (error) {
         console.error("error al conectarse al FakeAPI");
-    }
+    }          
 }
 getProducts();
+
+
 
