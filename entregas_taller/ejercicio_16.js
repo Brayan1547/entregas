8487c4106769e634dd6bc9c58ffd8c76ae8cc9f1
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-04-30 21:26:18 -0500

    ejercicio 16

---
+++ entregas_taller/ejercicio_16.js
@@ -0,0 +1,11 @@
+
+let num1 = Number(prompt("dijite un numero"));
+let num2 = Number(prompt("dijite el otro numero"));
+
+if (num1 > num2) {
+    console.log("el mayor es " + num1)
+}else if(num2 > num1){
+    console.log("el mayor es " + num2 )
+}else{
+    console.log("son iguales")
+};
