0859491e51ca226f26bd5718d82e66251c6fab3c
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-04-30 20:09:48 -0500

    ejercicio 10 y 10v2

--- entregas_taller/ejercicio_10_v2.js
+++ entregas_taller/ejercicio_10_v2.js
@@ -1,4 +1,4 @@
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
-const unir = array1.concat(array2);
+const unir = [ ...array1, ...array2 ];  // Spread
 console.log(unir);
---
+++ entregas_taller/ejercicio_11.js
@@ -0,0 +1,7 @@
+let personas = [
+  { nombre: "Juan", edad: 20, ciudad: "Bogotá" },
+  { nombre: "Ana", edad: 25, ciudad: "Lima" },
+  { nombre: "Carlos", edad: 30, ciudad: "Madrid" },
+  { nombre: "Luisa", edad: 22, ciudad: "México" },
+  { nombre: "Pedro", edad: 28, ciudad: "Buenos Aires" }
+];
