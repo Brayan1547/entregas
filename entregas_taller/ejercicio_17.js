ca76e576621598320535e8b9e129bcb4c23ec241
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-04-30 21:40:53 -0500

    ejercicio 17

---
+++ entregas_taller/ejercicio_17.js
@@ -0,0 +1,11 @@
+let usuario = Number(prompt("ingresa un numero"));
+
+
+if (usuario > 0) {
+    console.log("es positivo")
+} else if (usuario < 0){
+    console.log("es negativo")
+} else {
+    console.log("es cero")
+};
+
