a9495f3df3d1d062091b6ebe191478bc4548cec6
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-05-07 21:06:05 -0500

    ejercicio 3

---
+++ funciones_taller_2/funciones_3.js
@@ -0,0 +1,16 @@
+function sumaImpares(numeros) {
+    let suma = 0;
+
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros [i] % 2 !== 0) {
+            suma = suma + numeros[i]
+        }
+        
+    }
+    return suma;
+}
+
+console.log (
+    // sumaImpares([1, 2, 3, 4, 5]) 
+    sumaImpares([10, 15, 20, 25])
+)
