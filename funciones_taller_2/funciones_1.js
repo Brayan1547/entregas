c060902e83f07e4b04ad9778febf223f210d1b3e
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-05-07 19:22:15 -0500

    ejercicio 1

---
+++ funciones_taller_2/funciones_1.js
@@ -0,0 +1,12 @@
+function paresEnRango(inicio, fin){
+    let pares = [];
+    
+    for (let i = inicio; i <= fin; i++) {
+        if (i % 2 === 0) {
+            pares.push(i)
+        } 
+    }
+    return pares;
+}
+
+console.log(paresEnRango(1,10));
